e48b3af22afd30fa34565632510235ee1ec452f9
James Cheney <jamesdcheney0@gmail.com>
2022-08-09 12:23:33 -0700

    organized and added more values

--- variables.tf
+++ variables.tf
@@ -3,22 +3,43 @@ variable "aws_region" {
     type = string
 }
 
+
+variable "ec2_instance_name" {
+    description = "Instance name for EC2 instance"
+    type = string
+}
+variable "asg_instance_name" {
+    description = "Instance name for ASG instances"
+    type = string
+}
+
+
 variable "ec2_instance_type" {
     description = "Instance type for EC2 instance"
     type = string
 }
-
-variable "ec2_storage_size" {
-    description = "Instance type for EC2 instance"
-    type = string
-}
-
-variable "ASG_instance_type" {
+variable "asg_instance_type" {
     description = "Instance type for ASG instances"
     type = string
 }
 
-variable "asg_storage_size" {
-    description = "Instance type for ASG instances"
+
+variable "ec2_volume_size" {
+    description = "Instance volume size for EC2 instance"
+    type = string
+}
+variable "asg_volume_size" {
+    description = "Instance volume size for ASG instances"
+    type = string
+}
+
+
+
+variable "ec2_volume_type" {
+    description = "Instance volume type for EC2 instances"
+    type = string
+}
+variable "asg_volume_type" {
+    description = "Instance volume type for ASG instances"
     type = string
 }
