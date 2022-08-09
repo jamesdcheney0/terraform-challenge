d8daa5190ed74cedc8ab1d0f54beeaa288afac04
James Cheney <jamesdcheney0@gmail.com>
2022-08-09 12:24:21 -0700

    pulls rhel ami and uses vars for everything else

---
+++ ec2/main.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "rhel_8_5" {
+  most_recent = true
+  owners = ["309956199498"] 
+  filter {
+    name   = "name"
+    values = ["RHEL-8.5*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.rhel_8_5.id
+  instance_type = var.ec2_instance_type
+  subnet_id              = var.public_subnet_1_id
+  vpc_security_group_ids = var.bastion_ssh_sg
+  root_block_device {
+    volume_size = var.ec2_volume_size
+    volume_type = var.ec2_volume_type
+  }
+
+  tags = {
+    Name = var.ec2_instance_name
+  }
+}
