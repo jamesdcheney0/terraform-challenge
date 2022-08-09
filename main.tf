34595152ea337f7dcb862492db716d48062ecdb2
James Cheney <jamesdcheney0@gmail.com>
2022-08-09 12:23:06 -0700

    continued variablization efforts

--- main.tf
+++ main.tf
@@ -23,13 +23,12 @@ module "aws_security_groups" {
 
 module "aws_ec2" {
     source = "./ec2"
+    ec2_instance_name = var.ec2_instance_name
     ec2_instance_type = var.ec2_instance_type
-    ec2_storage_size = var.ec2_storage_size
+    ec2_volume_size = var.ec2_volume_size
+    ec2_volume_type = var.ec2_volume_type
     vpc_id = module.vpc.vpc_id
     public_subnet_1_id = module.vpc.public_subnet_1_id
-    public_subnet_2_id = module.vpc.public_subnet_2_id
-    private_subnet_1_id = module.vpc.private_subnet_1_id
-    private_subnet_2_id = module.vpc.private_subnet_2_id
 }
 
 module "aws_s3" {
@@ -38,10 +37,16 @@ module "aws_s3" {
 
 module "aws_autoscaling" {
     source = "./autoscaling-group"
+    asg_instance_name = var.asg_instance_name
     ec2_instance_type = var.asg_instance_type
-    ec2_storage_size = var.asg_storage_size
+    ec2_volume_size = var.asg_volume_size
+    ec2_volume_type = var.asg_volume_type
     vpc_id = module.vpc.vpc_id
     subnet_id = module.vpc.subnet_id
+
+    public_subnet_2_id = module.vpc.public_subnet_2_id
+    private_subnet_1_id = module.vpc.private_subnet_1_id
+    private_subnet_2_id = module.vpc.private_subnet_2_id
 }
 
 module "aws_alb" {
