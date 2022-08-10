0bfbe9824128ea7e3beaa953d6d8cfbaceeb6dbf
James Cheney <jamesdcheney0@gmail.com>
2022-08-10 13:51:51 -0700

    missing id on subnet value

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -79,7 +79,7 @@ resource "aws_eip" "nat_gateway_eip" {
 
 resource "aws_nat_gateway" "nat_gateway" {
     allocation_id = aws_eip.nat_gateway_eip.id
-    subnet_id = aws_subnet.public_subnet_1
+    subnet_id = aws_subnet.public_subnet_1.id
 
     tags = {
         Name = var.nat_gateway_name
