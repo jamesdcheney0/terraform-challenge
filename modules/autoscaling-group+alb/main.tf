2fca19eb79c5672bf86bbac4e2c4e2968999f631
James Cheney <jamesdcheney0@gmail.com>
2022-08-10 13:24:20 -0700

    name space needed "" and mispelled autoscaling

--- modules/autoscaling-group+alb/main.tf
+++ modules/autoscaling-group+alb/main.tf
@@ -65,7 +65,7 @@ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
     comparison_operator = "GreaterThanOrEqualToThreshold"
     evaluation_periods = "2"
     metric_name = "CPUUtilization"
-    namespace = AWS/EC2
+    namespace = "AWS/EC2"
     period = "60"
     statistic = "Average"
     threshold = "10"
@@ -73,7 +73,7 @@ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
         "${aws_autoscaling_policy.web_server_asg_policy.arn}"
     ]
     dimensions = {
-        AutoScalingGroupName = "${aws_autoscling_group.web_server_asg.name}"
+        AutoScalingGroupName = "${aws_autoscaling_group.web_server_asg.name}"
     }
 }
 
