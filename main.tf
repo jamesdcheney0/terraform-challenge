24068b9a1d5f4c6512c68c5bc32d28297f620057
James Cheney <jamesdcheney0@gmail.com>
2022-08-10 13:40:25 -0700

    added s to credentials file

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   region = var.aws_region
   shared_config_files = ["~/.aws/config"]
-  shared_credential_files = ["~/.aws/credentials"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile = "personal-aws"
 }
 
