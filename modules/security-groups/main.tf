e6aa8745c9f858aaaed92ec647e2ca387fd72816
James Cheney <jamesdcheney0@gmail.com>
2022-08-10 13:05:36 -0700

    updated the sg file to include http and ssh

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "web_server" {
-  name        = var.ec2_security_group_name
-  description = var.ec2_security_group_description
+  name        = var.web_server_sg_name
+  description = var.web_server_sg_description
   vpc_id      = var.vpc_id
 
   ingress {
@@ -9,7 +9,6 @@ resource "aws_security_group" "web_server" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -17,11 +16,34 @@ resource "aws_security_group" "web_server" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = var.ec2_security_group_name
+    Name = var.web_server_sg_name
   }
 }
 
+resource "aws_security_group" "bastion_ssh" {
+  name        = var.bastion_ssh_sg_name
+  description = var.bastion_ssh_sg_description
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "Allow traffic on port 22 from local machine"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["98.168.57.5/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.bastion_ssh_sg_name
+  }
+}
