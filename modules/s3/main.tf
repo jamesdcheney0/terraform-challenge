07dcac6b8586b286fefdd7155615c00be682b8b8
James Cheney <james.cheney@di2e.net>
2022-12-05 09:21:43 -0700

    replaced acl w aws_s3_bucket_acl resource

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
-  acl    = "private"
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl = "private"
 }
