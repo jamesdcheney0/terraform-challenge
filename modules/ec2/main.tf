b09e9f310a5e05e753a9689d50394d9a61afc74f
James Cheney <james.cheney@di2e.net>
2022-12-05 09:46:41 -0700

    corrected data inputs

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -22,8 +22,8 @@ data "aws_ami" "rhel_8_5" {
 resource "aws_instance" "bastion" {
   ami                    = data.aws_ami.rhel_8_5.id
   instance_type          = var.ec2_instance_type
-  subnet_id              = var.public_subnet
-  vpc_security_group_ids = var.bastion_sg_id
+  subnet_id              = var.public_subnet.id
+  vpc_security_group_ids = [var.bastion_sg_id]
   key_name               = var.ec2_public_key
   root_block_device {
     volume_size = var.ec2_volume_size
